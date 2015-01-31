27efcee3c9105a3d7cd34acdc5e43c687b9da555
Daniel Rodriguez <daniel.rodriguezli86@gmail.com>
2015-01-31 12:42:35 -0500

    Added create and index

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,9 +2,17 @@ class RequestsController < ApplicationController
   before_action :authenticate_coach!
 
   def index
+    requests = Request.where( status: 1 ) 
   end
 
   def create
+    request = Request.new(request_params)
+    if request.save
+      redirect_to root_path
+    else
+      flash[:error] = 'request culdnt be craated'
+      redirect_to root_patht
+    end 
   end
 
   def edit
