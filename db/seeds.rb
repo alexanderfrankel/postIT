33d3c2307397d04b593b512fc7b8702a49ac4216
Alwell <robinalwell@Robins-MacBook-Pro.local>
2015-01-31 15:15:09 -0500

    Mock Data seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ locations = %w(A3 A4 A5 A6 A8 A9 A1 A2 B4 B3 B2 B1 B5 B6 B7 B8 B9 C1 C2 C3 C4)
+ statuses = %w(1 2 3)
+ topics = %w(heroku activerecord js angular meteor bootstrap databases grammar cats)
+request_arr = []
+ 15.times {
+ 	r = Request.create(student: Faker::Name.name, location: locations.pop, status: statuses.sample, tag: topics.sample)
+ 	request_arr << r
+ }
+
+ 3.times {
+ 	coach = Coach.create(email: Faker::Internet.email, password: 'password')
+ 	3.times {
+ 		coach.requests << request_arr.pop
+ 	}
+ }
