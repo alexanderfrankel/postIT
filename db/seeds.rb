12f5b3c94cd5e0a7dd95be39766689d6403bd2d2
Alexander Frankel <alexxander.frankel@gmail.com>
2015-01-31 18:51:58 -0500

    Fix merge conflict

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
  locations = %w(A1 A2 A3 A4 A5 A6 B1 B2 B3 B4 B5 B6 C1 C2 C3 C4 C5 C6 D1 D2 D3 D4 D5 D6 E1 E2 E3 E4 E5 F6 F1 F2 F3 F4 F5 F6)
-
+ locations.reverse!
  36.times {
  	Computer.create(location: locations.pop, status: 0)
  }
